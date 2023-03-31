5723a41c6b1cbad3ea9254bb93fb8ea996460caa
Салихов Камиль Ильгизович <ksalikhov@phoenixit.ru>
2023-03-31 10:52:08 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import "./App.css";
 import { subscribe } from "../push";
 
+const title = "Push notification";
+const options = {
+    body: "test push"
+ };
+
+navigator.serviceWorker.ready.then(function(serviceWorker) {
+  serviceWorker.showNotification(title, options);
+});
+
 function App() {
   return (
     <>
